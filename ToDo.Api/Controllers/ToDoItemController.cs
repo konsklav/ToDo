d5f48f7d38f34fa354620652fdc33ab1bcbdbf20
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-04 18:49:45 +0200

    Add ToDoItemController.cs actions.

--- ToDo.Api/Controllers/ToDoItemController.cs
+++ ToDo.Api/Controllers/ToDoItemController.cs
@@ -6,4 +6,27 @@ namespace ToDo.Api.Controllers;
 [Route("todos/{toDoId:int}/items")]
 public class ToDoItemController : ControllerBase
 {
+    [HttpGet("{itemId:int}")]
+    public async Task<IResult> Get(int itemId, int toDoId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IResult> Create(int toDoId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{itemId:int}")]
+    public async Task<IResult> Update(int itemId, int toDoId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{itemId:int}")]
+    public async Task<IResult> Delete(int itemId, int toDoId)
+    {
+        throw new NotImplementedException();
+    }
 }
