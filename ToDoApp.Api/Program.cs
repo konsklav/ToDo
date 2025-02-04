3490eba0c27613835ad5851b97881f813588c7d5
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-04 19:42:59 +0200

    Configure appsettings.json and add environment variable prefix

--- ToDoApp.Api/Program.cs
+++ ToDoApp.Api/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables("TODO_");
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ToDoContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
+});
 
 var app = builder.Build();
 
