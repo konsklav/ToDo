fb790bfd64fc5d1cfad728f3ee0fc56eda57c847
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-02-04 18:47:08 +0200

    Specified properties

--- ToDo.Api/Models/User.cs
+++ ToDo.Api/Models/User.cs
@@ -2,5 +2,15 @@ namespace ToDo.Api.Models;
 
 public class User
 {
-    
+    public string Username { get; private set; }
+    public string Password { get; private set; }
+
+    // Required for EFCore
+    private User() { }
+
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
 }
