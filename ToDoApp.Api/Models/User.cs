b9833713cf6ebdddffc7d03e520def818d700c91
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-02-04 22:49:23 +0200

    Added Id to User entity

--- ToDoApp.Api/Models/User.cs
+++ ToDoApp.Api/Models/User.cs
@@ -2,14 +2,16 @@ namespace ToDoApp.Api.Models;
 
 public class User
 {
+    public int Id { get; private set; }
     public string Username { get; private set; }
     public string Password { get; private set; }
 
     // Required for EFCore
     private User() { }
 
-    public User(string username, string password)
+    public User(string username, string password, int id = 0)
     {
+        Id = id;
         Username = username;
         Password = password;
     }
