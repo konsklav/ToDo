9caa890f617ef43414859badc380512d83e9a5d4
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-06 15:16:34 +0200

    Add helpful method to easily retrieve user ID from session

---
+++ ToDoApp.Api/HttpContextExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApp.Api;
+
+internal static class HttpContextExtensions
+{
+    public static int GetRequiredUserId(this HttpContext context) => 
+        context.Session.GetInt32(TodoConstants.SessionUserId)
+        ?? throw new InvalidOperationException("Couldn't find user ID in session. Please ensure you have middleware to" +
+                                               "check for session IDs.");
+}
