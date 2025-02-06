59eaa76bb6dc633f358bb419f6c3c2dae0896751
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-02-06 18:35:53 +0200

    Made ToDo.ToDoItems list readonly

--- ToDoApp.Api/Models/ToDo.cs
+++ ToDoApp.Api/Models/ToDo.cs
@@ -2,20 +2,22 @@ namespace ToDoApp.Api.Models;
 
 public class ToDo
 {
+    private readonly List<ToDoItem> _toDoItems;
+    
     public int Id { get; private set; }
     public string Title { get; private set; }
     public User CreatedBy { get; private set; }
-    public List<ToDoItem> ToDoItems { get; private set; }
+    public IReadOnlyList<ToDoItem> ToDoItems => _toDoItems;
     
     // Required for EFCore
     private ToDo() {}
 
-    public ToDo(string title, User createdBy, List<ToDoItem> toDoItems, int id = 0)
+    private ToDo(string title, User createdBy, List<ToDoItem> toDoItems, int id = 0)
     {
         Id = id;
         Title = title;
         CreatedBy = createdBy;
-        ToDoItems = toDoItems;
+        _toDoItems = toDoItems;
     }
 
     public void Update(string title)
