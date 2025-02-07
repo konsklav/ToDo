dee7b1b9fda3213478fb6c7ebcbe8030d64a2ee7
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-07 21:48:37 +0200

    Add XML docs

--- ToDoApp.Api/Program.cs
+++ ToDoApp.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using ToDoApp.Api;
 using ToDoApp.Api.Data.Repositories.ToDos;
@@ -14,14 +15,18 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xml = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xml));
+});
 builder.Services.AddScoped<ToDoRepository>();
 builder.Services.AddDbContext<ToDoContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
 builder.Services.AddScoped<SessionManagementMiddleware>();
 
 var app = builder.Build();
 
-app.MapOpenApi("/");
+app.UseSwagger();
 app.UseHttpsRedirection();
 app.UseSession();
 app.UseMiddleware<SessionManagementMiddleware>();
